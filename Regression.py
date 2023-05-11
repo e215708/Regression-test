cc375a9f5c3ad1e299eeb229e3b8fe4f5652aab4
SK <s@SKnoMacBook-Air.local>
2023-05-11 13:58:27 +0900

    first commit

--- Regression.py
+++ Regression.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-def load_linear_example():
-    X = np.array([[1,4],[1,8],[1,13],[1,17]])
-    Y = np.array([7,10,11,14])
-    return X,Y
+class LinearRegression:
+    x = None
+    theta = None
+    y = None
 
-if __name__ == "__main__":
-    X,Y = load_linear_example()
-    print(f'{X=}')
-    print(f'{Y=}')
+    def fit(self,x,y):
+        pass
+
+    def predict(self,x):
+        pass
+
+    def score(self,x,y):
+        pass
